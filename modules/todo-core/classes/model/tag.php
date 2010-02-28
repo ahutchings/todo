d23b7cb342040688972622de86e71153cbe91040
Andrew Hutchings <andrew.hutchings@gmail.com>
2010-02-28 03:25:15 -0500

    Add validation rules to models.

--- modules/todo-core/classes/model/tag.php
+++ modules/todo-core/classes/model/tag.php
@@ -3,4 +3,7 @@
 class Model_Tag extends ORM
 {
     protected $_has_many = array('tasks' => array('through' => 'tags_tasks'));
+
+    protected $_rules = array('name' => array('not_empty' => NULL));
+    protected $_filters = array(TRUE => array('trim' => NULL));
 }
--- modules/todo-core/classes/model/task.php
+++ modules/todo-core/classes/model/task.php
@@ -3,4 +3,7 @@
 class Model_Task extends ORM
 {
     protected $_has_many = array('tags' => array('through' => 'tags_tasks'));
+
+    protected $_rules = array('content' => array('not_empty' => NULL));
+    protected $_filters = array(TRUE => array('trim' => NULL));
 }
