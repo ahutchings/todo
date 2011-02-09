a08bf1dea351eeb7d748d8cc12360b8002e4a55d
Andrew Hutchings <andrew.hutchings@gmail.com>
2011-02-09 02:35:46 -0500

    Update task model filters and rules syntax for 3.1

--- modules/todo-core/classes/model/task.php
+++ modules/todo-core/classes/model/task.php
@@ -4,9 +4,28 @@ class Model_Task extends ORM
 {
     protected $_has_many = array('tags' => array('through' => 'tags_tasks'));
 
-    protected $_rules = array('name' => array('not_empty' => NULL));
-    protected $_filters = array(TRUE => array('trim' => NULL));
-
-	protected $_created_column = array('column' => 'created_at', 'format' => TRUE);
+    protected $_created_column = array('column' => 'created_at', 'format' => TRUE);
 	protected $_updated_column = array('column' => 'updated_at', 'format' => TRUE);
+
+    public function rules()
+    {
+        return array(
+            'name' => array(
+                array('not_empty'),
+            ),
+            'user_id' => array(
+                array('not_empty'),
+                array('digit'),
+            ),
+        );
+    }
+
+    public function filters()
+    {
+        return array(
+            TRUE => array(
+                array('trim'),
+            ),
+        );   
+    }
 }
